1fda0d55b2a3f62f658f4be03e3311c49e49e7a1
Daniel Aimacaña <daniel.aimacana@epn.edu.ec>
2023-08-01 21:36:27 -0500

    delete member

--- src/api/groups.ts
+++ src/api/groups.ts
@@ -170,7 +170,6 @@ export const deleteGroupById = async (groupId: string): Promise<void> => {
 }
 
 export const deleteMemberById = async(memberId: string, groupId: string): Promise<void> => {
-  const memberCollection = groupCollection.doc(groupId).collection("members");
   await memberCollection.doc(memberId).delete();
 }
 
